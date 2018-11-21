905a749dbe8dc892da2c62070d19b0aa4260fa86
Mahesh-wor <maheshwor89@gmail.com>
2018-11-21 22:27:43 +0545

    added threading capabilities

--- dota2ping.py
+++ dota2ping.py
@@ -1,31 +1,68 @@
 import sys
 import subprocess
 import os
+import threading
 
-sevip = {'sea':'103.28.54.1','euw':'146.66.152.1',
-'eue':'146.66.155.1','usw':'192.69.96.1',
-'use':'208.78.164.1','au':'103.10.125.1',
-'ru':'146.66.156.1','same':'209.197.29.1',
-'saf':'197.80.200.1','dubai':'185.25.183.1'}
+sevip = {'SE Asia-1':'sgp-1.valve.net',
+'Europe West':'lux.valve.net',
+'Europe East':'vie.valve.net',
+'US West':'eat.valve.net',
+'US East':'iad.valve.net',
+'Australia':'syd.valve.net',
+'Russia':'sto.valve.net',
+'South America':'gru.valve.net',
+'South Africa-1':'196.38.180.1',
+'South Africa-2':'197.80.200.1',
+'South Africa-3':'197.84.209.1',
+'Dubai':'185.25.183.1',
+'SE Asia-2':'sgp-2.valve.net',
+'India':'116.202.224.146',
+}
+art="""
+▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒
+▓▓▓▒░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▒░░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░▒▓▓▓▓▓▓▓
+▓▓▓▓▓▒░░░░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒░░░░▒▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▒░░░░░▒▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░▒▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▒░░░░░░▒▓▓▓▓▓▓▓▓▓▓▓▒▒░░░░░░▒▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▒░░░░░░░▒▓▓▓▓▓▓▓▓▓▓▓▓▒░░░▒▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░▒▓▓▓▓▓▓▓▓▓▓▒░▒▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░▒▓▓▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓▒░░▒▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░▒▓▓▓▓▓▓▓
+▓▓▓▓▓▓▒░░░░░▒▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░░▒▓▓▓▓▓
+▓▓▓▓▓▒░░░░░░░░▒▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░░▒▓▓▓
+▓▓▓▓▒░░░░░░░░░░░▒▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░▒▓▓
+▓▓▓▓▓▒░░░░░░░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░░░░░░░▒▓▓▓
+▓▓▓▓▓▓▒░░░░▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░░░░░▒▓▓▓▓
+▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒"""
 
-print(sevip['same'])
-print(sevip['sea'])
+#print(sevip['same'])
+#print(sevip['sea'])
 
-for sev,ip in sevip.items():
+def pingserver(ip,sev):
     ping_response = subprocess.Popen(["/bin/ping","-c1", ip], stdout=subprocess.PIPE).stdout.read()
     latency = str(ping_response)
-    if len(latency) > 5:
-        print(len(latency))
-        ping_value = 'Error'
-    else:
-        ping_value=latency.split('ms\\n\\n---')[0].split('=')[-1]
-    print(ping_value)
+    ping_value=latency.split('ms\\n\\n---')[0].split('=')[-1]
 
+    if len(ping_value) > 10:
+        #print('length',len(ping_value))
+        ping_value = 'Error / Server_Mismatch'
+        print(sev,'----------------',ping_value)
+    else:
+        print(sev,'----------------',ping_value)
+
+threads_list = []
+for sev,ip in sevip.items():
+    t = threading.Thread(target=pingserver,name='thread_{}'.format(sev),args=(ip,sev))
+    threads_list.append(t)
+    t.start()
+
+
+    #print('{} has started'.format(t.name))
 
 
 #    ping_list_dict['sev']=ping_value
 #    print(ping_list_dict)
-
-
-
-input()
--- thread_test.py
+++ thread_test.py
@@ -1,15 +1,27 @@
 import threading
-
 import time
-
 
 def sleeper (n, name):
     print ('hi am {}. goint to sleep for 5 sec \n'.format(name))
     time.sleep(n)
     print ('{} has woke up from sleep \n'.format(name))
-
-
+"""
 t = threading.Thread(target = sleeper,name='thread1',
-                     args = (2,'thread1'))
+                     args = (5,'thread1'))
 
 t.start()
+
+"""
+threads_list = []
+start = time.time()
+for i in range(5):
+    t = threading.Thread(target=sleeper,name='thread{}'.format(i),args=(5,'thread{}'.format(i)))
+    threads_list.append(t)
+    t.start()
+    print('{} has started'.format(t.name))
+
+for t in threads_list:
+    t.join()
+end = time.time()
+print('time tanken:{}'.format(end-start))
+print('All Tthreads finished job')
